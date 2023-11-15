3b4fef55203a7d28f0ae40aa6c9153ca8366d948
Sabuz <everythingkst@gmail.com>
2023-11-16 02:04:43 +0600

    vueJs project added

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue';
+import TheWelcome from '../components/TheWelcome.vue'
+const counter = ref(0);
+
+
+</script>
+
+<template>
+  <main>
+    <!-- <TheWelcome /> -->
+    <h1>{{  counter }}</h1>
+    <button @click="counter++">+</button>
+    <button @click="counter--">-</button>
+  </main>
+</template>
